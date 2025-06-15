0bfe1b4e02f8c4a1b8ba49f0d24c57f2322af98e
nurturenature <jeff.suttor@earthlink.net>
2025-06-14 20:35:22 -0700

    add Clojure 1.12.1 as a dep.

--- jepsen-control/project.clj
+++ jepsen-control/project.clj
@@ -1,3 +1,4 @@
 (defproject jepsen-docker "0.1.0-SNAPSHOT"
   :description "Jepsen setup for docker."
-  :dependencies [[jepsen "0.3.9"]])
+  :dependencies [[org.clojure/clojure "1.12.1"]
+                 [jepsen "0.3.9"]])
