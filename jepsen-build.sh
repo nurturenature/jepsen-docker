f1af0ea0a9d181ab3c7a5e44a5cb05708e5bc164
Jeff Suttor <jeff.suttor@earthlink.net>
2026-03-02 14:49:16 -0800

    remove --progress quiet from Docker builds

--- jepsen-build.sh
+++ jepsen-build.sh
@@ -2,9 +2,9 @@
 set -e
 
 # build Jepsen images
-docker build --progress quiet -t jepsen-setup:bookworm   ./jepsen-setup
-docker build --progress quiet -t jepsen-node:bookworm    ./jepsen-node
-docker build --progress quiet -t jepsen-control:bookworm ./jepsen-control
+docker build -t jepsen-setup:bookworm   ./jepsen-setup
+docker build -t jepsen-node:bookworm    ./jepsen-node
+docker build -t jepsen-control:bookworm ./jepsen-control
 
 echo
 echo "Jepsen Docker images have been built"
